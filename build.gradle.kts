6691e5475f9b8dbbe0c9c2bf3aa75a0b75a01203
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2024-04-09 16:58:11 -0500

    returned build.gradle file to its original condition

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.8.0"
     application
     id("io.gitlab.arturbosch.detekt") version "1.23.3"
 }
@@ -20,13 +20,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(20)
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(20))
-    }
+    jvmToolchain(11)
 }
 
 detekt {
